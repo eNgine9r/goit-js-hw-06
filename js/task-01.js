fbfc1a19818762b0aca33e177e84ef45d09da2b3
Serhii Tymchuk <92324923+eNgine9r@users.noreply.github.com>
2023-01-30 22:47:06 +0200

    Update task-01.js
    update v 011

--- js/task-01.js
+++ js/task-01.js
@@ -5,6 +5,6 @@ const titleEl = document.querySelector("h2");
 console.log('Category:', titleEl.textContent);
 
 const listEl = document.querySelectorAll('.item, h2, ul');
-console.log('Elements:', listEl[h2])
+console.log('Elements:', listEl)
 
 
