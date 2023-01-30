28b9841ba03497766811c263232183657e4d4780
Serhii Tymchuk <92324923+eNgine9r@users.noreply.github.com>
2023-01-30 22:46:32 +0200

    Update task-01.js
    update v 01

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const liItemEl = document.querySelectorAll('.item');
+console.log('Number of categories:', liItemEl.length);
+
+const titleEl = document.querySelector("h2");
+console.log('Category:', titleEl.textContent);
+
+const listEl = document.querySelectorAll('.item, h2, ul');
+console.log('Elements:', listEl[h2])
+
+
