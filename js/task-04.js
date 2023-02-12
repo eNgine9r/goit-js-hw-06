d444f03249ec5da4db25ea81ac85f730a384a45d
Serhii Tymchuk <92324923+eNgine9r@users.noreply.github.com>
2023-02-12 21:19:05 +0200

    Done

--- js/task-04.js
+++ js/task-04.js
@@ -1,6 +1,3 @@
-// 1. потрібно щоб при натиснення на кнопку +1 змінювався спан на +1 число
-// 2. при натиснені кнопки -1 з числа яке збережене у спані віднімалась одиниця
-
 const decrementBtn = document.querySelector("button[data-action='decrement']");
 const incrementBtn = document.querySelector("button[data-action='increment']");
 const valueNum = document.querySelector("#value");
