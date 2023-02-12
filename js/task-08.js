c0c89f47ec30a3f1d64df6b81d5aeaf89a7d8e74
Serhii Tymchuk <92324923+eNgine9r@users.noreply.github.com>
2023-02-12 21:18:26 +0200

    Update task 09 / done

--- js/task-08.js
+++ js/task-08.js
@@ -1,27 +1,6 @@
-// const form = document.querySelector('.login-form');
-
-// form.addEventListener('submit', onFormSubmit)
-
-// function onFormSubmit (event) {
-//     event.preventDefault();
-
-//     // const formElements = event.currentTarget.elements;
-//     // console.dir(formElements);
-
-//     // const mail = formElements.email.value;
-//     // const password = formElements.password.value;
-
-//     // console.log(mail, password);
-//     const formData = new FormData(event.currentTarget);
-//     formData.forEach((value, name) => {
-//         console.log('onFormSubmit -> name', name);
-//         console.log('onFormSubmit -> value', value);
-//     });
-// };
-
 
 const form = document.querySelector(".login-form");
-const loginFormSubmitHeandler = (event) => {
+const loginFormSubmit = (event) => {
   event.preventDefault();
   const email = event.currentTarget.elements.email.value;
   const password = event.currentTarget.elements.password.value;
@@ -34,4 +13,4 @@ const loginFormSubmitHeandler = (event) => {
     : console.log(formData);
   event.currentTarget.reset();
 };
-form.addEventListener("submit", loginFormSubmitHeandler);
+form.addEventListener("submit", loginFormSubmit);
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const spanText = document.querySelector('.color');
+const buttonClick = document.querySelector('.change-color');
+const widgetBody = document.querySelector('.widget');
+
+buttonClick.addEventListener('click', onInputChange);
+function onInputChange () {
+  widgetBody.style.backgroundColor = getRandomHexColor();
+  spanText.textContent = widgetBody.style.backgroundColor;
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
